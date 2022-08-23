0caa52cb51f9d69aa69b46f054269c7aff6e1670
fenin <kongkenan@gmail.com>
2022-08-23 12:08:51 -0700

    basic design

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.Home.as_view(), name='home'),
+    path('champions/', views.Champions.as_view(), name='champions'),
+    path('augments/', views.Augments.as_view(), name='augments'),
+    path('traits/', views.TraitsList.as_view(), name='traits_list'),
 ]

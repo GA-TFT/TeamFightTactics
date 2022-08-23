eef0e73b7483f32c20a3d4433d2daebdd877b0f1
brianthoeurn <brianmonirath@hotmail.com>
2022-08-23 09:43:35 -0700

    Hiding environmental variables

--- leagueoflegends/teamfighttactics/settings.py
+++ leagueoflegends/teamfighttactics/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=e#qpyto+9jg=ab5^=t*9a*g(_(qa#n0n)_3gxct$5z+g1e7nn'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = True if os.getenv('MODE') == 'dev' else False
 
 ALLOWED_HOSTS = []
 

eb11f6293b14882a92c3fdc13e09a045d9fcce6c
fenin <kongkenan@gmail.com>
2022-08-23 16:13:37 -0700

    paths

--- main_app/views.py
+++ main_app/views.py
@@ -11,8 +11,17 @@ class Home(TemplateView):
 class Champions(TemplateView):
     template_name = 'champions.html'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["champions"] = Champions
+        return context
+
 class Augments(TemplateView):
     template_name = 'augments.html'
 
 class TraitsList(TemplateView):
     template_name = 'traits.html'
+
+# class Champions(View):
+#     def get(self, request):
+#         return HttpResponse("Champions Page")

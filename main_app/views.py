f79befff5adae12f5e18974e373c841a92f02dc9
brianthoeurn <brianmonirath@hotmail.com>
2022-08-24 13:30:24 -0700

    rebase

--- main_app/views.py
+++ main_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponse
 from django.views.generic.base import TemplateView
+import json
 
 # Create your views here.
 

8dc3169ecdd3cd3c2485b508aa663ffafe680384
fenin <kongkenan@gmail.com>
2022-08-24 10:57:09 -0700

    add league imgs

--- main_app/models.py
+++ main_app/models.py
@@ -2,7 +2,7 @@ from multiprocessing.dummy import Array
 from django.db import models
 
 # Create your models here.
-class Champion(models.Model):
-    ability = Array
-    cost = models.IntegerField()
-    name = models.CharField()
+# class Champion(models.Model):
+#     ability = Array
+#     cost = models.IntegerField()
+#     name = models.CharField()

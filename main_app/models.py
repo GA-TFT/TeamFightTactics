f2f02a4715536bc4588774ebdc419c74e53f549b
brianthoeurn <brianmonirath@hotmail.com>
2022-08-24 13:30:24 -0700

    rebase

--- main_app/models.py
+++ main_app/models.py
@@ -2,7 +2,20 @@ from multiprocessing.dummy import Array
 from django.db import models
 
 # Create your models here.
-# class Champion(models.Model):
-#     ability = Array
-#     cost = models.IntegerField()
-#     name = models.CharField()
+class Champion(models.Model):
+    ability = models.CharField(max_length=100)
+    abilname = models.CharField(max_length=100)
+    abilicon = models.CharField(max_length=100)
+    cost = models.IntegerField()
+    name = models.CharField(max_length=100)
+    icon = models.CharField(max_length=100)
+    armor = models.IntegerField()
+    attack_speed = models.DecimalField('Attack Speed', decimal_places=2, max_digits=3)
+    damage = models.IntegerField()
+    hp = models.IntegerField()
+    initialmama = models.IntegerField('Initial Mana')
+    magic_resist = models.IntegerField('Magic Resist')
+    mana = models.IntegerField()
+    range = models.IntegerField()
+    traits = models.CharField(max_length=100)
+

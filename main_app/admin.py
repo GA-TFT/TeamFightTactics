ca05df54ee3ce0a12d0fc9fe3a162dee7628a751
brianthoeurn <brianmonirath@hotmail.com>
2022-08-29 12:13:58 -0700

    admin

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from .models import Champion, Trait, Augment, Video
+from .models import Champion, Trait, Augment, Video, TraitImage
 # Register your models here.
 
 admin.site.register(Champion)
 admin.site.register(Trait)
 admin.site.register(Augment)
 admin.site.register(Video)
-# admin.site.register(TraitImage)
+admin.site.register(TraitImage)

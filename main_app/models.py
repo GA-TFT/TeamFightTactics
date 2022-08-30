2ef84f555e9e992ae9d333a683c3ab2fba9c5328
brianthoeurn <brianmonirath@hotmail.com>
2022-08-29 21:56:33 -0700

    revisions

--- main_app/models.py
+++ main_app/models.py
@@ -3,49 +3,6 @@ from django.db import models
 from embed_video.fields import EmbedVideoField
 
 # Create your models here.
-class Comp(models.Model):
-    name = models.CharField(max_length=500)
-    tier = models.CharField(max_length=500)
-    desc = models.CharField(max_length=500)
-    imgone = models.CharField(max_length=500)
-    imgtwo = models.CharField(max_length=500)
-    imgthree = models.CharField(max_length=500)
-    imgfour =models.CharField(max_length=500)
-    imgfive = models.CharField(max_length=500)
-    imgsix = models.CharField(max_length=500)
-    imgseven = models.CharField(max_length=500)
-    nameone = models.CharField(max_length=500)
-    nametwo = models.CharField(max_length=500)
-    namethree = models.CharField(max_length=500)
-    namefour =models.CharField(max_length=500)
-    namefive = models.CharField(max_length=500)
-    namesix = models.CharField(max_length=500)
-    nameseven = models.CharField(max_length=500)
-
-    def __str__(self):
-        return f'{self.name} ({self.id})'
-    
-class Trait(models.Model):
-    name = models.CharField(max_length=500)
-    desc = models.CharField("Description", max_length=900)
-    icon = models.CharField(max_length=100)
-    champions = models.CharField(max_length=100)
-
-    def __str__(self):
-        return f'{self.name} ({self.id})'
-
-    def get_absolute_url(self):
-        return reverse('detail', kwargs={'trait_id': self.id})
-
-    class Meta:
-        ordering = ['name']
-
-class Traitimg(models.Model):
-    charname = models.CharField(max_length=100)
-    image = models.ImageField(upload_to='main_app/static/images/home', blank=True)
-    charimage = models.ForeignKey(Trait, related_name='traitimage', on_delete=models.CASCADE)
-
-
 class Champion(models.Model):
     ability = models.CharField(max_length=500)
     abilname = models.CharField(max_length=100)
@@ -71,6 +28,23 @@ class Champion(models.Model):
     class Meta:
         ordering = ['name']
 
+class Trait(models.Model):
+    name = models.CharField(max_length=500)
+    desc = models.CharField("Description", max_length=900)
+    icon = models.CharField(max_length=100)
+    champions = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'{self.name} ({self.id})'
+
+    class Meta:
+        ordering = ['name']
+
+class Traitimg(models.Model):
+    charname = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='main_app/static/images/home', blank=True)
+    charimage = models.ForeignKey(Trait, related_name='traitimage', on_delete=models.CASCADE)
+
 class Augment(models.Model):
     name = models.CharField(max_length=500)
     tier = models.IntegerField()
@@ -83,10 +57,31 @@ class Augment(models.Model):
     def get_absolute_url(self):
         return reverse('detail', kwargs={'augment_id': self.id})
     
-
     class Meta:
         ordering = ['name']
 
+class Comp(models.Model):
+    name = models.CharField(max_length=500)
+    tier = models.CharField(max_length=500)
+    desc = models.CharField(max_length=500)
+    imgone = models.CharField(max_length=500)
+    imgtwo = models.CharField(max_length=500)
+    imgthree = models.CharField(max_length=500)
+    imgfour =models.CharField(max_length=500)
+    imgfive = models.CharField(max_length=500)
+    imgsix = models.CharField(max_length=500)
+    imgseven = models.CharField(max_length=500)
+    nameone = models.CharField(max_length=500)
+    nametwo = models.CharField(max_length=500)
+    namethree = models.CharField(max_length=500)
+    namefour =models.CharField(max_length=500)
+    namefive = models.CharField(max_length=500)
+    namesix = models.CharField(max_length=500)
+    nameseven = models.CharField(max_length=500)
+
+    def __str__(self):
+        return f'{self.name} ({self.id})'
+    
 class Video(models.Model):
     title = models.CharField(max_length=100)
     url = EmbedVideoField()
